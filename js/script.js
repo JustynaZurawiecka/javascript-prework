75e521d10b1a63f5ed4f235d360e304f8896e632
Justyna Żurawiecka <justyna2106@wp.pl>
2020-07-05 22:12:18 +0200

    Add buttons for rock, paper and scissors

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
-function getMoveName(argMoveId) {
-    if(argMoveId == 1){
-        return 'kamień'
-    } else if(argMoveId == 2) {
-        return 'papier';  
-    } else if(argMoveId == 3) {
-        return 'nożyce';
+function playGame(playerInput) {
+    clearMessages();
+    function getMoveName(argMoveId) {
+        if(argMoveId == 1){
+            return 'kamień'
+        } else if(argMoveId == 2) {
+            return 'papier';  
+        } else if(argMoveId == 3) {
+            return 'nożyce';
+        }
+        return 'nieznany ruch';
     }
-    return 'nieznany ruch';
+    
+    let playerMove = getMoveName(playerInput);  
+
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    let computerMove = getMoveName(randomNumber);
+    
+    function displayResult(argComputerMove, argPlayerMove){
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+      
+        if((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ){
+        printMessage('Ty wygrywasz!');
+        } else if((argComputerMove == argPlayerMove) ){
+        printMessage('Remis');
+        } else if(argPlayerMove == 'nieznany ruch') {
+        printMessage('Niedozwolone!');
+        } else {
+        printMessage('Tym razem przegrywasz :(');
+        }
+      }
+    
+    return displayResult(computerMove,playerMove);
 }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-let playerMove = getMoveName(prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'));
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+});    
 
-function displayResult(argComputerMove, argPlayerMove){
-    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  
-    if((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ){
-    printMessage('Ty wygrywasz!');
-    } else if((argComputerMove == argPlayerMove) ){
-    printMessage('Remis');
-	} else if(argPlayerMove == 'nieznany ruch') {
-    printMessage('Niedozwolone!');
-	} else {
-    printMessage('Tym razem przegrywasz :(');
-	}
-  }
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+});
 
-printMessage(displayResult(computerMove,playerMove));
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+});
